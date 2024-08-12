67a234c47a76294ff5e4dafe1c236e4a026fb3ab
OpusTheMoose <36777623+OpusTheMoose@users.noreply.github.com>
2024-08-11 18:37:17 -0600

    Delete out directory

--- out/build/default/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o: \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/src/SFML/Graphics/Vertex.cpp \
- /usr/include/stdc-predef.h \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/Graphics/Vertex.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/Config.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/Graphics/Color.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/System/Vector2.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/System/Vector2.inl
--- out/build/default/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.o: \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/src/SFML/System/Sleep.cpp \
- /usr/include/stdc-predef.h \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/System/Sleep.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/Config.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/include/SFML/System/Time.hpp \
- /home/opusthemoose/Fractal/out/build/default/_deps/sfml-src/src/SFML/System/Unix/SleepImpl.hpp
